897e9926a347df55ea52df5afc6c37cc748d861a
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-23 22:15:03 -0300

    Example
    Example from the book C++ Primer Plus about combination types in one
    program

---
+++ mixtypes/mixtypes/mixtypes.cpp
@@ -0,0 +1,29 @@
+// some type combinations
+#include<iostream>
+
+struct antartica_years_end
+{
+
+	int year;
+
+};
+
+int main() 
+{
+	antartica_years_end s01, s02, s03;
+	s01.year = 1998;
+	antartica_years_end *pa = &s02;
+	pa->year = 1999;
+	antartica_years_end trio[03];
+	trio[0].year = 2003;
+	std::cout << trio->year << std::endl;
+	const antartica_years_end *arp[3] = { &s01, &s02, &s03 };
+	std::cout << arp[1]->year << std::endl;
+	const antartica_years_end **ppa = arp;
+	auto ppb = arp; //C++ 11 automatic type declaration // or else use const antartica_years_end ** ppb = arp
+	std::cout << (*ppa)->year << std::endl;
+	std::cout << (*(ppb + 1))->year << std::endl;
+
+	return 0;
+
+}
